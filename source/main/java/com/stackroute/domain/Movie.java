6cf775d783d951ac39526bba92c06dce14e164cc
Magnolia JR <magnoliajr18@gmail.com>
2019-07-15 08:12:47 +0530

    Update movie

--- source/main/java/com/stackroute/domain/Movie.java
+++ source/main/java/com/stackroute/domain/Movie.java
@@ -13,7 +13,27 @@ public class Movie {
     }
 
     public void display(){
+package com.stackroute.domain;
 
+public class Movie {
+
+   private Actor actorr;
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "actor=" + actorr +
+                '}';
+    }
+
+    public Actor getActorr() {
+        return actorr;
+    }
+
+    public void setActorr(Actor actorr) {
+        this.actorr = actorr;
+    }
+}
 
         System.out.println("Actor name: "+actor.getName()+"\nAge : "+actor.getAge()+"\nGender: "+actor.getGender());
     }
