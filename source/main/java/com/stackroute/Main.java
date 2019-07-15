a10b3267ee08bfedc6113c2a1b0f59ef24407cf3
Magnolia JR <magnoliajr18@gmail.com>
2019-07-15 08:24:21 +0530

    Update Main.java eigth commit

--- source/main/java/com/stackroute/Main.java
+++ source/main/java/com/stackroute/Main.java
@@ -1,3 +1,4 @@
+
 package com.stackroute;
 
 import com.stackroute.domain.Movie;
@@ -26,18 +27,14 @@ public class Main {
 
 
         Movie movie = context.getBean("movie", Movie.class);
+//        Movie movie2 = context.getBean("movie2", Movie.class);
 
         System.out.println(movie);
+//        System.out.println(movie2);
 
-        XmlBeanFactory xmlBeanFactory= new XmlBeanFactory ( new ClassPathResource("beans.xml"));
-        Movie movieFirst= (Movie)xmlBeanFactory.getBean("movie");
 
-        movie.setApplicationContext(context);
-        movieFirst.setBeanFactory(xmlBeanFactory);
-        movie.setBeanName("beam123");
 
 
     }
 
 }
-©
