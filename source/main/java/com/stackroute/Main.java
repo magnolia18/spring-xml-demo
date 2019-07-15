15675b26273b494ce8345a467988b8d7e8bfe1d8
Magnolia JR <magnoliajr18@gmail.com>
2019-07-15 08:19:56 +0530

    Update main.java seventh commit

--- source/main/java/com/stackroute/Main.java
+++ source/main/java/com/stackroute/Main.java
@@ -1,27 +1,43 @@
-
 package com.stackroute;
 
-import com.stackroute.demo.BeanLifeCycleDemo;
-import com.stackroute.demo.BeanPostProcessorDemoBean;
-import com.stackroute.domain.Employee;
+import com.stackroute.domain.Movie;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionReader;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.support.SimpleBeanDefinitionRegistry;
+import org.springframework.beans.factory.xml.ResourceEntityResolver;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+
+import java.sql.SQLOutput;
 
 public class Main {
 
-    public  static  void main(String args []) {
+    public static void main(String[]args) {
 
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+//using application context
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 
-        ((ClassPathXmlApplicationContext) applicationContext).registerShutdownHook();
 
-        BeanLifeCycleDemo beanLifeCycleDemo = (BeanLifeCycleDemo) applicationContext.getBean("cycle");
+        Movie movie = context.getBean("movie", Movie.class);
 
-        BeanPostProcessorDemoBean beanPostProcessorDemoBean = applicationContext.getBean("postprocessing",BeanPostProcessorDemoBean.class);
+        System.out.println(movie);
 
-        Employee emp = applicationContext.getBean("childemployee",Employee.class);
+        XmlBeanFactory xmlBeanFactory= new XmlBeanFactory ( new ClassPathResource("beans.xml"));
+        Movie movieFirst= (Movie)xmlBeanFactory.getBean("movie");
 
-        System.out.println("Bean definition inheritance:");
-        System.out.println(emp.toString());
+        movie.setApplicationContext(context);
+        movieFirst.setBeanFactory(xmlBeanFactory);
+        movie.setBeanName("beam123");
+
+
     }
+
 }
+©
