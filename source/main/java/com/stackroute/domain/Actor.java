22bc58052e63f3e04db11d4a7d83a583515ef448
Magnolia JR <magnoliajr18@gmail.com>
2019-07-15 08:15:40 +0530

    Sixth commit

--- source/main/java/com/stackroute/domain/Actor.java
+++ source/main/java/com/stackroute/domain/Actor.java
@@ -1,7 +1,12 @@
+package com.stackroute.domain;
+
+public class Actor {
+
+   private String name;
+
     public Actor(String name, String gender, int age) {
         this.name = name;
         this.gender = gender;
-        this.age = age;
     }
 
     @Override
@@ -9,6 +14,34 @@
         return "Actor{" +
                 "name='" + name + '\'' +
                 ", gender='" + gender + '\'' +
-                ", age=" + age +
                 '}';
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    private String gender;
+    private int age;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
