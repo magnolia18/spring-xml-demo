6e52c4b1a971f8ad3cce61dd1d1c2f7843a67524
Magnolia JR <magnoliajr18@gmail.com>
2019-07-15 08:32:07 +0530

    Update Main.java ninth commit

--- source/main/java/com/stackroute/Main.java
+++ source/main/java/com/stackroute/Main.java
@@ -1,16 +1,39 @@
 package com.stackroute;
 
 import com.stackroute.domain.Movie;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionReader;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.support.SimpleBeanDefinitionRegistry;
+import org.springframework.beans.factory.xml.ResourceEntityResolver;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+
+import java.sql.SQLOutput;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[]args) {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("com/stackroute/beans.xml");
-        System.out.println("Beans.xml file loaded.");
+//using application context
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+
         Movie movie = context.getBean("movie", Movie.class);
-        movie.readInfo();
+//        Movie movie2 = context.getBean("movie2", Movie.class);
+
+        System.out.println(movie);
+//        System.out.println(movie2);
+
+
+
+
     }
+
 }
