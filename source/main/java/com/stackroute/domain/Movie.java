9c3c2df83b0af2c2682a75cbb773c3f6a020ef55
Magnolia JR <magnoliajr18@gmail.com>
2019-07-15 08:22:08 +0530

    Update Movie.java Seventh commit

--- source/main/java/com/stackroute/domain/Movie.java
+++ source/main/java/com/stackroute/domain/Movie.java
@@ -1,8 +1,22 @@
 package com.stackroute.domain;
 
-public class Movie {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Movie implements ApplicationContextAware, BeanFactoryAware, BeanNameAware {
 
    private Actor actorr;
+    private ApplicationContextAware applicationContextAware;
+    private  BeanFactoryAware beanFactoryAware;
+    private  BeanNameAware beanNameAware;
+
+    public Movie(Actor actorr) {
+        this.actorr = actorr;
+    }
 
     @Override
     public String toString() {
@@ -18,5 +32,20 @@ public class Movie {
     public void setActorr(Actor actorr) {
         this.actorr = actorr;
     }
-}
 
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println(beanFactory);
+    }
+
+    @Override
+    public void setBeanName(String s) {
+        System.out.println(s);
+
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println(applicationContext);
+    }
+}
