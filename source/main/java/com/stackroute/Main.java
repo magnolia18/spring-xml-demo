8f68ce4149edb205525fea8db4d719531dea31d9
Magnolia JR <magnoliajr18@gmail.com>
2019-07-15 08:18:41 +0530

    Update main.java sixth commit

--- source/main/java/com/stackroute/Main.java
+++ source/main/java/com/stackroute/Main.java
@@ -1,51 +1,27 @@
+
 package com.stackroute;
 
-import com.stackroute.domain.Movie;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.support.BeanDefinitionReader;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.support.SimpleBeanDefinitionRegistry;
-import org.springframework.beans.factory.xml.ResourceEntityResolver;
-import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
+import com.stackroute.demo.BeanLifeCycleDemo;
+import com.stackroute.demo.BeanPostProcessorDemoBean;
+import com.stackroute.domain.Employee;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.FileSystemResource;
-
-import java.sql.SQLOutput;
 
 public class Main {
 
-    public static void main(String[]args) {
+    public  static  void main(String args []) {
 
-//using application context
-        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
 
-//scope=singleton
-        Movie movie1 = context.getBean("movie1", Movie.class);
-        movie1.display();
+        ((ClassPathXmlApplicationContext) applicationContext).registerShutdownHook();
 
-        Movie movieB = context.getBean("movie1", Movie.class);
-        movie1.display();
+        BeanLifeCycleDemo beanLifeCycleDemo = (BeanLifeCycleDemo) applicationContext.getBean("cycle");
 
-        Movie movie2 = context.getBean("movie2", Movie.class);
-        movie2.display();
+        BeanPostProcessorDemoBean beanPostProcessorDemoBean = applicationContext.getBean("postprocessing",BeanPostProcessorDemoBean.class);
 
-        Movie movie3 = context.getBean("movie3", Movie.class);
-        movie3.display();
-        
-//scope= prototype
-        Movie movie4 = context.getBean("movie4", Movie.class);
-        movie4.display();
+        Employee emp = applicationContext.getBean("childemployee",Employee.class);
 
-        Movie movie5 = context.getBean("movie4", Movie.class);
-        movie5.display();    //gives null
-        
-        System.out.println(movie1 == movieB);
-        
-
+        System.out.println("Bean definition inheritance:");
+        System.out.println(emp.toString());
     }
-
+}
